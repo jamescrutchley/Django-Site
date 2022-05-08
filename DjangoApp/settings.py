8a655e3d46f39f1c1203e7552a4218ee68430716
jamescrutchley <103486271+jamescrutchley@users.noreply.github.com>
2022-05-08 13:13:22 +1000

    fixing typos & directory structure

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testapp.app.TestappConfig'
+    'testapp.apps.TestappConfig'
 ]
 
 MIDDLEWARE = [
--- testapp/urls.py
+++ testapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 # #####:8000/test
 urlpatterns = [
     path('',views.test_view),
-    path('/todo',views.to_do_view)
+    path('todo/',views.to_do_view,name='todo'),
 ]
