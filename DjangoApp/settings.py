cc3ad72ef44ff3011c7e877b5dfb65c17e3d3fb1
jamescrutchley <103486271+jamescrutchley@users.noreply.github.com>
2022-05-11 13:16:41 +1000

    Update settings.py

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'DjangoApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates'), os.path.join(BASE_DIR,'templates/registration')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'), os.path.join(BASE_DIR,'testapp/templates/testapp')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
