f49f7ed39e42ecfc12bd46475eda627e73001ea1
James Crutchley <jamescrutchley98@gmail.com>
2022-05-19 14:39:21 +1000

    typo

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 import testapp
-from . import views
+from .views import *
 from django.views.generic import RedirectView
 
 app_name = 'todo'
 
 urlpatterns = [
-    path('',index,name='home'), # display list
+    path('', index, name='home'), # display list
     path('add-task/',add_task,name='add-task'), # add new tasks
     path('edit-task/<int:pk>/',edit_task,name='edit-task'), # edit tasks
     path('add-subtask/<int:pk>/',add_subtask,name='add-subtask'), # add subtasks to tasks
