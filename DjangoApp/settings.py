b7f7174d4c4d9abc65235c5edf4faadaf55c9a60
James Crutchley <jamescrutchley98@gmail.com>
2022-05-19 12:18:53 +1000

    settings

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testapp.apps.TestappConfig'
+    'testapp.apps.TestappConfig',
+    'todo.apps.TodoConfig',
 ]
 
 MIDDLEWARE = [
--- testapp/urls.py
+++ testapp/urls.py
@@ -5,5 +5,4 @@ from django.views.generic import RedirectView
 # #####:8000/test
 urlpatterns = [
     path('',views.test_view),
-    path('todo/',views.to_do_view,name='todo'),
 ]
