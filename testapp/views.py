db9481824867c3d62d5a02cf9b2774851166e8d2
James Crutchley <jamescrutchley98@gmail.com>
2022-05-19 12:21:46 +1000

    test_view

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def test_view(request):
-    return render(request,'base.html')
+    return HttpResponse('Testapp, test_view')
 
 def to_do_view(request):
     return render(request,'todo.html')
